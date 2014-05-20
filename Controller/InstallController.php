663075d092f82ffd0fdd9d7c1767e911e7878138
topaz2 <topaz2.3333@gmail.com>
2014-05-20 14:15:17 +0900

    Fixed redirect action

--- Controller/InstallController.php
+++ Controller/InstallController.php
@@ -103,13 +103,12 @@ class InstallController extends InstallAppController {
 			if ($this->request->is('post')) {
 				$this->User->create();
 				if ($this->User->save($this->request->data)) {
-					return $this->redirect(array('action' => 'index'));
+					return $this->redirect(array('action' => 'finish'));
 				} else {
 					$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 				}
 			}
-
-			$this->redirect(array('action' => 'finish'));
+			$this->redirect(array('action' => __FUNCTION__));
 		}
 	}
 
