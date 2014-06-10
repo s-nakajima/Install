3f1ed1c0779b8f46b9bfcb4aba3766e11252d1fe
topaz2 <topaz2.3333@gmail.com>
2014-06-10 14:21:23 +0900

    Added @codeCoverageIgnore

--- Test/Case/AllInstallTest.php
+++ Test/Case/AllInstallTest.php
@@ -6,6 +6,7 @@ class AllInstallTest extends CakeTestSuite {
  *
  * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
  * @return CakeTestSuite
+ * @codeCoverageIgnore
  */
 	public static function suite() {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
