f4160d3e24b3d69579cec74b7fe13cdec39ba159
Shohei Nakajima <nakajimashouhei@gmail.com>
2015-08-02 06:41:51 +0900

    Fix for migration order

--- Controller/InstallController.php
+++ Controller/InstallController.php
@@ -323,7 +323,7 @@ class InstallController extends InstallAppController {
 			}
 
 			$plugins = array_unique(array_merge(
-				array('NetCommons', 'DataTypes', 'Users', 'M17n', 'PluginManager', 'Roles'),
+				array('NetCommons', 'Users', 'M17n', 'DataTypes', 'PluginManager', 'Roles'),
 				App::objects('plugins'),
 				array_map('basename', glob(ROOT . DS . 'app' . DS . 'Plugin' . DS . '*', GLOB_ONLYDIR))
 			));
