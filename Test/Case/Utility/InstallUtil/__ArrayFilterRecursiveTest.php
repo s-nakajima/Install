b03c760436020bee4a1aeddc5678dcc2b557d8f2
s-nakajima <nakajimashouhei@gmail.com>
2016-09-02 18:59:22 +0900

    InstallUtil::__arrayFilterRecursive()のテスト

---
+++ Test/Case/Utility/InstallUtil/__ArrayFilterRecursiveTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * InstallUtil::__arrayFilterRecursive()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsCakeTestCase', 'NetCommons.TestSuite');
+App::uses('InstallUtil', 'Install.Utility');
+
+/**
+ * InstallUtil::__arrayFilterRecursive()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Install\Test\Case\Utility\InstallUtil
+ */
+class PrivateInstallUtilityInstallUtilArrayFilterRecursiveTest extends NetCommonsCakeTestCase {
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'install';
+
+/**
+ * __arrayFilterRecursive()のテスト
+ *
+ * @return void
+ */
+	public function test__arrayFilterRecursive() {
+		//データ生成
+		$input = array('aaaa', array('object' => new InstallUtil()));
+		$callback = function ($val) {
+			return !is_object($val);
+		};
+
+		//テスト実施
+		$instance = new InstallUtil();
+		$result = $this->_testReflectionMethod(
+			$instance, '__arrayFilterRecursive', array($input, $callback)
+		);
+
+		//チェック
+		$expected = array(
+			'aaaa', array()
+		);
+		$this->assertEquals($expected, $result);
+	}
+
+}
